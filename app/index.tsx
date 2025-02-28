6952e24d88f5e4a66840b63871c3e48af4576589
Ijerson Lastimosa Ilalto <77521841+sonlast@users.noreply.github.com>
2025-02-28 12:02:07 +0800

    commit for fonts config

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,44 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import React  from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+// import { useFonts } from 'expo-font';
+import LinearGradient_ from '../components/LinearGradient_';
+// import * as SplashScreen from 'expo-splash-screen';
 
-export default class index extends Component {
-  render() {
-    return (
+const index = () => {
+  // const [fontsLoaded, fontsError] = useFonts({
+  //   'Snippet-Regular': require('../assets/fonts/Snippet-Regular.ttf'),
+  // });
+
+  // useEffect(() => {
+  //   if (fontsLoaded || fontsError) {
+  //     SplashScreen.hideAsync();
+  //   }
+  // }, [fontsLoaded, fontsError]);
+
+  // if (!fontsLoaded && !fontsError) {
+  //   return null;
+  // }
+
+  return (
+    <View style={styles.container}>
+      <LinearGradient_ />
       <View>
-        <Text> textInComponent </Text>
+        <Text style={styles.appName}> AR FitCoach </Text>
       </View>
-    )
-  }
+    </View>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  appName: {
+    fontSize: 40,
+    fontFamily: 'Snippet-Regular'
+  }
+})
+
+export default index;
--- constants/Colors.tsx
+++ constants/Colors.tsx
@@ -1,12 +1,21 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+const tintColorLight = '#0a7ea4';
+const tintColorDark = '#fff';
 
-export default class Colors extends Component {
-  render() {
-    return (
-      <View>
-        <Text> textInComponent </Text>
-      </View>
-    )
-  }
-}
+export const Colors = {
+  light: {
+    text: '#11181C',
+    background: '#fff',
+    tint: tintColorLight,
+    icon: '#687076',
+    tabIconDefault: '#687076',
+    tabIconSelected: tintColorLight,
+  },
+  dark: {
+    text: '#ECEDEE',
+    background: '#151718',
+    tint: tintColorDark,
+    icon: '#9BA1A6',
+    tabIconDefault: '#9BA1A6',
+    tabIconSelected: tintColorDark,
+  },
+};
