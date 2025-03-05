989fe543bd70b18e80912c9ce3ef3b7bc5d596df
Ijerson Lastimosa Ilalto <77521841+sonlast@users.noreply.github.com>
2025-03-05 17:11:15 +0800

    commit for build

--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,77 @@
-import React from 'react';
-import { Pressable, StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Alert, Pressable, StyleSheet, Text, View } from 'react-native';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faMicrophone, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { Audio } from 'expo-av';
 import LinearGradient_ from '../components/LinearGradient_';
+
 import { Fonts } from '../constants/Fonts';
 
 const index = () => {
+  const [isRecording, setIsRecording] = useState(false);
+  const [recording, setRecording] = useState<Audio.Recording | null>(null);
+
+  const startRecording = async () => {
+    try {
+      console.log("Starting recording...");
+      const { granted } = await Audio.requestPermissionsAsync();
+      if (!granted) {
+        Alert.alert('Permission to access microphone was denied');
+        return;
+      }
+
+      const recording = new Audio.Recording();
+      await recording.prepareToRecordAsync({
+        ...Audio.RecordingOptionsPresets.HIGH_QUALITY,
+        isMeteringEnabled: true,
+      });
+      await recording.startAsync();
+      setRecording(recording);
+      setIsRecording(true);
+      console.log("Recording started");
+    } catch (err) {
+      console.error('Failed to start recording', err);
+    }
+  };
+
+  const stopRecording = async () => {
+    try {
+      console.log("Stopping recording...");
+      if (!recording) return;
+
+      await recording.stopAndUnloadAsync();
+      const uri = recording.getURI();
+      console.log("Recording stopped and stored at", uri);
+      setRecording(null);
+      setIsRecording(false);
+      console.log("Recording stopped");
+    } catch (err) {
+      console.error('Failed to stop recording', err);
+    }
+  };
+
+  useEffect(() => {
+    const requestPermission = async () => {
+      const { granted } = await Audio.requestPermissionsAsync();
+      if (!granted) {
+        Alert.alert('Permission to access microphone was denied');
+      }
+    }
+    requestPermission();
+  }, []);
+
   console.log(`REVAI API KEY: ${process.env.EXPO_PUBLIC_REVAI_API}`)
   return (
     <View style={styles.container}>
       <LinearGradient_ />
       <View style={styles.container}>
         <Text style={styles.appName}> AR FitCoach </Text>
-        <Pressable onPress={() => console.log("Speak")} style={styles.speakButton}>
-          <FontAwesomeIcon icon={faMicrophone} size={50} style={{ color: "#000" }} />
+        <Pressable onPress={isRecording ? stopRecording : startRecording } style={styles.speakButton}>
+          <FontAwesomeIcon icon={faMicrophone} size={50} style={{ color: isRecording ? "#f00" : "#000" }} />
         </Pressable>
         <View style={{ marginTop: 40 }}>
           <Text style={styles.basicText}>
-            TAP TO SPEAK
+            {isRecording ? "TAP TO STOP" : "TAP TO SPEAK"}
           </Text>
         </View>
         <View style={{ marginTop: 40 }}>
