e063df84106229640bddef2503f8fbc05c6db56d
Ijerson Lastimosa Ilalto <77521841+sonlast@users.noreply.github.com>
2025-03-11 18:11:31 +0800

    added a statusbar config

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,7 +2,7 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-// import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import { Appearance } from 'react-native';
 import 'react-native-reanimated';
@@ -35,16 +35,18 @@ export default function RootLayout() {
 
   return (
     // <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <>
       <Stack 
         screenOptions={{
           headerStyle: { backgroundColor: theme.backgroundHeader },
         }}
-      > 
+        > 
         <Stack.Screen name="index" options={{ headerShown: false }}/>
         <Stack.Screen name="search" options={{ headerShown: false }}/>
         <Stack.Screen name="+not-found" options={{ headerShown: false }}/>
       </Stack>
-      // <StatusBar style="dark" />
+      <StatusBar style="dark" />
+    </>
     // </ThemeProvider>
   );
 }
