b12dea3c65e3b5d92302f29d523d55b85220bcbe
Ijerson Lastimosa Ilalto <77521841+sonlast@users.noreply.github.com>
2025-03-12 17:39:01 +0800

    modified for adaptability to background image

--- components/LinearGradient_.tsx
+++ components/LinearGradient_.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet } from 'react-native'
 import { LinearGradient } from 'expo-linear-gradient'
 
 const LinearGradient_ = () => {
@@ -22,6 +22,7 @@ const styles = StyleSheet.create({
     right: 0,
     top: 0,
     height: '100%',
+    zIndex: 1,
   },
 })
 
