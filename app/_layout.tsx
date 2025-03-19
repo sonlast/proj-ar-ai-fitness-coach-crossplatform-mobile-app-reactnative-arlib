7f47bfbd6d75421347f388f206cfbb4fa496f265
Ijerson Lastimosa Ilalto <77521841+sonlast@users.noreply.github.com>
2025-03-19 16:25:36 +0800

    removed unnecessary codes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,4 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+// import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
@@ -43,6 +43,7 @@ export default function RootLayout() {
         > 
         <Stack.Screen name="index" options={{ headerShown: false }}/>
         <Stack.Screen name="search" options={{ headerShown: false }}/>
+        {/* <Stack.Screen name="signup" options={{ headerShown: false }}/> */}
         <Stack.Screen name="+not-found" options={{ headerShown: false }}/>
       </Stack>
       <StatusBar style="dark" />
