3f1dc2def6a3d77c336c067c3806b7348dc91729
Ijerson Lastimosa Ilalto <77521841+sonlast@users.noreply.github.com>
2025-04-03 14:53:36 +0800

    modified modals path

--- constants/Sheets.tsx
+++ constants/Sheets.tsx
@@ -1,5 +1,5 @@
 import { registerSheet, SheetDefinition } from "react-native-actions-sheet";
-import ModalSheet from "@/components/ModalSheet";
+import ModalSheet from "@/components/modals/bottom-modals/ModalActionsSheet";
 
 registerSheet('modalSheet', ModalSheet);
 
