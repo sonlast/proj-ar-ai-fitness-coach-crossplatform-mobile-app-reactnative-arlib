cc21c373c9c5cdd23dcf8417646b81141061558e
Ijerson Lastimosa Ilalto <77521841+sonlast@users.noreply.github.com>
2025-04-10 14:37:25 +0800

    modified to use style props

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,26 +1,20 @@
 import React from 'react'
-import { StyleSheet } from 'react-native'
+import { StyleProp, ViewStyle } from 'react-native'
 import LottieView from 'lottie-react-native'
 
-const Loading = () => {
+type LoadingProps = {
+  loaderStyle?: StyleProp<ViewStyle>;
+}
+
+const Loading = ({ loaderStyle }: LoadingProps) => {
   return (
     <LottieView
       source={require('../assets/loaders/loader_droplets.json')}
       autoPlay
       loop
-      style={styles.loader}
+      style={loaderStyle}
     />
   )
 }
-
-const styles = StyleSheet.create({
-  loader: {
-    width: 140,
-    height: 140,
-    marginTop: 40,
-    marginBottom: 40,
-    alignSelf: "center"
-  }
-})
 
 export default Loading;
