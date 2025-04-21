e4f276ae7cf42e6030e20e6e4362ef52a813ca5f
Ijerson Lastimosa Ilalto <77521841+sonlast@users.noreply.github.com>
2025-04-21 16:11:26 +0800

    style: add button styles for demo / track

--- components/modals/pop-up-modals/ModalRNPaper.tsx
+++ components/modals/pop-up-modals/ModalRNPaper.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Image, Pressable, StyleSheet, Text, View } from 'react-native';
-import { Portal, Modal } from 'react-native-paper';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faXmark } from '@fortawesome/free-solid-svg-icons';
-import { Fonts } from '@/constants/Fonts';
-import { workoutImages } from '@/constants/Workout';
-import { WorkoutData } from '@/types/workout';
+import {Image, Pressable, StyleSheet, Text, ToastAndroid, View} from 'react-native';
+import {Portal, Modal} from 'react-native-paper';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faXmark} from '@fortawesome/free-solid-svg-icons';
+import {Fonts} from '@/constants/Fonts';
+import {workoutImages} from '@/constants/Workout';
+import {WorkoutData} from '@/types/workout';
 
 type ModalRNPaperProps = {
   visible: boolean;
@@ -13,8 +13,8 @@ type ModalRNPaperProps = {
   selectedWorkout?: WorkoutData | null;
 }
 
-const ModalRNPaper = ({ visible, onDismiss, selectedWorkout }: ModalRNPaperProps) => {
-  const workoutImage = selectedWorkout?.title 
+const ModalRNPaper = ({visible, onDismiss, selectedWorkout}: ModalRNPaperProps) => {
+  const workoutImage = selectedWorkout?.title
     ? workoutImages[selectedWorkout.title as keyof typeof workoutImages]
     : require('@/assets/images/icon.png')
 
@@ -35,6 +35,11 @@ const ModalRNPaper = ({ visible, onDismiss, selectedWorkout }: ModalRNPaperProps
           width: '100%',
           height: '30%',
         }}
+        theme={{
+          colors: {
+            backdrop: '#rgba(3, 3, 3, 0.6)',
+          }
+        }}
       >
         <View style={styles.modalContainer}>
           <View style={styles.modalContent}>
@@ -46,11 +51,29 @@ const ModalRNPaper = ({ visible, onDismiss, selectedWorkout }: ModalRNPaperProps
             <Text style={styles.modalDescription}>
               {selectedWorkout?.workoutDesc || 'No description available.'}
             </Text>
+            <View style={styles.viewDemoAndTrackButton}>
+              <Pressable
+                onPress={() => ToastAndroid.show('Demo Pressed', ToastAndroid.SHORT)}
+                style={styles.modalDemoAndTrackButton}
+              >
+                <Text style={styles.textDemoAndTrack}>
+                  DEMO
+                </Text>
+              </Pressable>
+              <Pressable
+                onPress={() => ToastAndroid.show('Track Pressed', ToastAndroid.SHORT)}
+                style={styles.modalDemoAndTrackButton}
+              >
+                <Text style={styles.textDemoAndTrack}>
+                  TRACK
+                </Text>
+              </Pressable>
+            </View>
             <Pressable
               style={styles.modalCloseButton}
               onPress={onDismiss}
             >
-              <FontAwesomeIcon icon={faXmark} size={20} color="#fff" />
+              <FontAwesomeIcon icon={faXmark} size={20} color="#fff"/>
             </Pressable>
           </View>
         </View>
@@ -75,7 +98,7 @@ const styles = StyleSheet.create({
     padding: 25,
     backgroundColor: 'rgba(0, 0, 0, 0.9)',
     shadowColor: '#fff',
-    shadowOffset: { width: 0, height: 2 },
+    shadowOffset: {width: 0, height: 2},
     shadowOpacity: 0.8,
     shadowRadius: 10,
     borderWidth: 1,
@@ -112,6 +135,26 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     padding: 5,
   },
+  viewDemoAndTrackButton: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginTop: 30,
+    marginHorizontal: 10
+  },
+  modalDemoAndTrackButton: {
+    backgroundColor: '#fff',
+    padding: 25,
+    paddingHorizontal: 35,
+    borderRadius: 100,
+    borderBottomWidth: 2,
+    borderRightWidth: 2,
+    borderColor: '#333',
+  },
+  textDemoAndTrack: {
+    fontFamily: Fonts.mainFont,
+    fontSize: 15,
+  }
 })
 
 export default ModalRNPaper;
