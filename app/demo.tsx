e80b9ede8f93b7bf675c69da95bd9c495c73573c
Ijerson Lastimosa Ilalto <77521841+sonlast@users.noreply.github.com>
2025-04-23 21:42:44 +0800

    feat: navigate to application settings using a button when camera permissions is denied

--- app/demo.tsx
+++ app/demo.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect } from 'react';
-import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { Linking, Platform, Pressable, StyleSheet, Text, View } from 'react-native';
 import { useCameraPermissions } from "expo-camera";
 import { Fonts } from '@/constants/Fonts';
 import { CAMERA_FACES } from '@/constants/CameraFaces';
 import { FUNCTION_TYPE } from '@/constants/FunctionType';
 import DemoOrTrack from '@/components/DemoOrTrack';
+import { startActivityAsync, ActivityAction } from 'expo-intent-launcher';
 
 const Demo = () => {
   const [permission, requestPermission] = useCameraPermissions({});
@@ -13,11 +14,22 @@ const Demo = () => {
     requestPermission();
   }, []);
   
+  const navigatePermissions = async () => {
+    if (Platform.OS === 'android') {
+      await startActivityAsync(
+        ActivityAction.APPLICATION_DETAILS_SETTINGS,
+        { data: 'package:' + 'com.i_son.lastimosa.fitness_app_ar'} 
+      ); 
+    } else {
+      await Linking.openSettings();
+    }
+  }
+
   if (!permission?.granted) {
     return (
       <View style={styles.container}>
         <View style={styles.buttonContainer}>
-          <Pressable style={styles.button} onPress={requestPermission}>
+          <Pressable style={styles.button} onPress={navigatePermissions}>
             <Text style={styles.fallbackText}>Grant Camera Permissions</Text>
           </Pressable>
         </View>
