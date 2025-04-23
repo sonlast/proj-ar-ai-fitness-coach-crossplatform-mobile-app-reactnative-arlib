ea7f8a8125076ccabd768185d087337fc264c7d9
Ijerson Lastimosa Ilalto <77521841+sonlast@users.noreply.github.com>
2025-04-23 14:24:28 +0800

    feat: implement request permission for camera usage

--- app/demo.tsx
+++ app/demo.tsx
@@ -1,12 +1,64 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useEffect } from 'react';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { useCameraPermissions } from "expo-camera";
+import { Fonts } from '@/constants/Fonts';
+import { CAMERA_FACES } from '@/constants/CameraFaces';
+import { FUNCTION_TYPE } from '@/constants/FunctionType';
+import DemoOrTrack from '@/components/DemoOrTrack';
 
 const Demo = () => {
+  const [permission, requestPermission] = useCameraPermissions({});
+
+  useEffect(() => {
+    requestPermission();
+  }, []);
+  
+  if (!permission?.granted) {
+    return (
+      <View style={styles.container}>
+        <View style={styles.buttonContainer}>
+          <Pressable style={styles.button} onPress={requestPermission}>
+            <Text style={styles.fallbackText}>Grant Camera Permissions</Text>
+          </Pressable>
+        </View>
+    </View>
+    );
+  }
+
   return (
-    <Text>
-      Demo
-    </Text>
+    <DemoOrTrack functionType={FUNCTION_TYPE.DEMO} cameraFace={CAMERA_FACES.BACK}/>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+    backgroundColor: '#000',
+    padding: 10,
+    borderRadius: 10,
+    margin: 5,
+    borderWidth: 1,
+    borderColor: 'white',
+  },
+  fallbackText: {
+    fontSize: 20,
+    fontFamily: Fonts.mainFont,
+    textAlign: 'center',
+    color: 'white',  
+    padding: 10,
+  }
+});
 
 export default Demo;
